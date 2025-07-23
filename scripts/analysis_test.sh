65c404bcf13fa5ae484cb00b2d68c464ae168ac7
Alejandro Gomez Espinosa <alejandro.gomez@cern.ch>
2025-07-23 16:27:59 -0400

    refactor output directory structure and update CI configurations for analysis tests

--- scripts/analysis_test.sh
+++ scripts/analysis_test.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 source scripts/set_initial_variables.sh --output ${1:-"bbww/output/"} --do_proxy
 
-OUTPUT_DIR="${DEFAULT_DIR}/analysis_test_job"
+OUTPUT_DIR="${DEFAULT_DIR}/analysis_test/"
 echo "############### Checking and creating output directory"
 if [ ! -d $OUTPUT_DIR ]; then
     mkdir -p $OUTPUT_DIR
--- scripts/analysis_test_cutflows.sh
+++ scripts/analysis_test_cutflows.sh
@@ -7,18 +7,20 @@ source scripts/set_initial_variables.sh --output ${1:-"bbww/output/"}
 
 # Define input and output paths
 # Create output directory if it doesn't exist
-OUTPUT_DIR="${DEFAULT_DIR}analysis_test_cutflow_job/"
+OUTPUT_DIR="${DEFAULT_DIR}/analysis_test_cutflows/"
 echo "############### Checking and creating output directory"
 if [ ! -d $OUTPUT_DIR ]; then
     mkdir -p $OUTPUT_DIR
 fi
-INPUT_FILE="${DEFAULT_DIR}analysis_test_job/test.coffea"
-OUTPUT_FILE="${OUTPUT_DIR}/test_cutflow.yml"
+INPUT_FILE="${DEFAULT_DIR}/analysis_test/test.coffea"
+OUTPUT_FILE="${OUTPUT_DIR}test_cutflow.yml"
 
 # Run the Python script
 cmd=(python bbww/tests/dump_cutflow_to_yaml.py -i $INPUT_FILE -o $OUTPUT_FILE)
 echo "Running: " "${cmd[@]}"
 "${cmd[@]}"
+ls -lh $OUTPUT_FILE
+cat $OUTPUT_FILE
 
 cmd=(python bbww/tests/cutflow_unittest.py --input_file $OUTPUT_FILE --known_cutflow bbww/tests/known_cutflow_hh_bbww_processor.yml)
 echo "Running: " "${cmd[@]}"

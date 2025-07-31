27dea1d84d62356cc9c481dadbeb71658e88796c
Alejandro Gomez Espinosa <alejandro.gomez@cern.ch>
2025-07-31 16:42:55 -0400

    fix: update output directory handling in skimmer_analysis_test script

--- scripts/skimmer_analysis_test.sh
+++ scripts/skimmer_analysis_test.sh
@@ -16,10 +16,14 @@ if [[ $(hostname) = *fnal* ]]; then
     echo "No changing files"
 else
     echo "############### Modifying previous dataset file (to read local files)"
-    sed -i "s|\/builds/$CI_PROJECT_PATH\/python\/||g" $INPUT_DIR/picoaod_datasets_GluGluToHHTo2B2VLNu2J_2022_preEE.yml
+    OUTPUT_BASE_DIR="/builds/$CI_PROJECT_PATH/python/"
+    INPUT_DIR=$OUTPUT_BASE_DIR/skimmer_test
+    OUTPUT_DIR=$OUTPUT_BASE_DIR/skimmer_analysis_test
+    sed -i "s|${OUTPUT_BASE_DIR}||g" $INPUT_DIR/picoaod_datasets_GluGluToHHTo2B2VLNu2J_2022_preEE.yml
     cat $INPUT_DIR/picoaod_datasets_GluGluToHHTo2B2VLNu2J_2022_preEE.yml
 fi
 echo "############### Modifying dataset file with skimmer ci output"
+echo "hostname: $(hostname)"
 cmd=(python metadata/merge_yaml_datasets.py -m $INPUT_DIR/datasets.yml -f $INPUT_DIR/picoaod_datasets_GluGluToHHTo2B2VLNu2J_2022_preEE.yml -o $OUTPUT_DIR/datasets.yml)
 echo "${cmd[@]}"
 "${cmd[@]}"

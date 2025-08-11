c4eecc1a03e60202c15e3aa42b1d9affe43d2cad
Aniket Khanal <akhanal@cmslpc344.fnal.gov>
2025-08-11 09:13:16 -0500

    fix cutflow output base

--- scripts/analysis_test_cutflows_run3.sh
+++ scripts/analysis_test_cutflows_run3.sh
@@ -6,14 +6,13 @@
 source "bbww/scripts/common.sh"
 
 # Parse output base argument
-OUTPUT_BASE_DIR=$(parse_output_base_arg "bbww/output/" "$@")
 if [ $? -ne 0 ]; then
     exit 1
 fi
 
 # Call the main analysis_test.sh script with Run3-specific parameters
 bash bbww/scripts/analysis_test_cutflows.sh \
-    --output-base "$OUTPUT_BASE_DIR" \
+    --output-base "bbww/output/" \
     --input-file "test.coffea" \
     --input-subdir "analysis_test_run3" \
     --output-filename "test_cutflow.yml" \

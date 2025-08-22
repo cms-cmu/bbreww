8f051036c5f35bb7e99a39aa0ff3e9bdbf5dac5e
Aniket Khanal <akhanal@cmslpc306.fnal.gov>
2025-08-22 11:18:45 -0500

    move cutflow table out of main processor

--- analysis/helpers/common.py
+++ analysis/helpers/common.py
@@ -176,6 +176,7 @@ def apply_jet_veto_maps( corrections_metadata, jets ):
 def get_sequential_cutflow(selection, events, selection_list):
     sequential_cutflow = {
         'events': {},
+        'weights': {}
     }
     cumulative_cuts = []
 
@@ -185,5 +186,38 @@ def get_sequential_cutflow(selection, events, selection_list):
         current_mask = selection.all(*cumulative_cuts)
         
         sequential_cutflow['events'][cut_name] = np.sum(current_mask)
+        sequential_cutflow['weights'][cut_name] = np.sum(events.weight[current_mask])
 
     return sequential_cutflow
+
+def add_output_cutflow(events, output):
+    region_map = {
+    'hadronic_W': events.channel.hadronic_W,
+    'leptonic_W': events.channel.leptonic_W,
+    'mu_region':  events.region.mu_region,
+    'e_region':   events.region.e_region
+    }
+    output['cutflow_weights'][events.metadata['dataset']] = {
+        name: {
+            'events': {
+                'preselection':      np.sum(events.selection.preselection[selector]),
+                'nominal_4j2b':      np.sum(events.selection.nominal_4j2b[selector]),
+                'nominal_3j1b':      np.sum(events.selection.nominal_3j2b[selector]),
+                'lowpt_4j2b':        np.sum(events.selection.lowpt_4j2b[selector]),
+                'chi_sq_nom_4j2b':   np.sum(events.selection.chi_sq_nom_4j2b[selector]),
+                'chi_sq_nom_3j2b':   np.sum(events.selection.chi_sq_nom_3j2b[selector]),
+                'chi_sq_lowpt_4j2b': np.sum(events.selection.chi_sq_lowpt_4j2b[selector])
+            },
+            'weights': {
+                'preselection':      np.sum(events.weight[events.selection.preselection[selector]]),
+                'nominal_4j2b':      np.sum(events.weight[events.selection.nominal_4j2b[selector]]),
+                'nominal_3j1b':      np.sum(events.weight[events.selection.nominal_3j2b[selector]]),
+                'lowpt_4j2b':        np.sum(events.weight[events.selection.lowpt_4j2b[selector]]),
+                'chi_sq_nom_4j2b':   np.sum(events.weight[events.selection.chi_sq_nom_4j2b[selector]]),
+                'chi_sq_nom_3j2b':   np.sum(events.weight[events.selection.chi_sq_nom_3j2b[selector]]),
+                'chi_sq_lowpt_4j2b': np.sum(events.weight[events.selection.chi_sq_lowpt_4j2b[selector]])
+            }
+        }
+        for name, selector in region_map.items()
+    }
+    return output

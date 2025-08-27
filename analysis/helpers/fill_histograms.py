2f11f3145b7fe1be8cd3fb7c5c7f61c9b89ab367
Aniket Khanal <akhanal@cmslpc306.fnal.gov>
2025-08-27 09:27:55 -0500

    add bjets pT histograms

--- analysis/helpers/fill_histograms.py
+++ analysis/helpers/fill_histograms.py
@@ -6,8 +6,8 @@ def fill_histograms(
     processName: str = None,
     year: str = 'UL18',
     is_mc: bool = False,
-    selection_list: list = ['basic_selection', 'preselection'],
-    channel_list: list = ['hadronic_W','leptonic_W', 'null'],
+    selection_list: list = ['preselection'],
+    channel_list: list = ['hadronic_W','leptonic_W'],
     region_list: list = ['e_region', 'mu_region']
 ):
 
@@ -28,9 +28,9 @@ def fill_histograms(
     fill += hist.add("nPVsGood", (101, -0.5, 100.5, ("PV.npvsGood", "Number of Good Primary Vertices")))
 
     ### gen studies plots ###
-    fill += hist.add("W_mass_res", (50, -100, 100, ("W_mass_res", "leptonic W mass resolution [GeV]")))
-    fill += hist.add("genW_mass", (50, 30, 150, ("genW_mass", "leptonic W mass resolution [GeV]")))
-    fill += hist.add("mlvqq_hadWs", (50, 50, 300, ("mlvqq_hadWs", "reconstructed H->lvqq mass [GeV]")))
+    #fill += hist.add("W_mass_res", (50, -100, 100, ("W_mass_res", "leptonic W mass resolution [GeV]")))
+    #fill += hist.add("genW_mass", (50, 30, 150, ("genW_mass", "leptonic W mass resolution [GeV]")))
+    #fill += hist.add("mlvqq_hadWs", (50, 50, 300, ("mlvqq_hadWs", "reconstructed H->lvqq mass [GeV]")))
     
     ### these histograms are just to study the quark vs. gluon selection efficiency
     #fill += hist.add("Wjets_pre_lead",    (15, 15 , 30,    ("Wjets_pre_lead.pt", r"$jet pT$[GeV]")))
@@ -43,28 +43,40 @@ def fill_histograms(
     #fill += hist.add("dijets_post_sublead",(15, 15 , 30, ("dijets_post_sublead.pt", r"$jet pT$[GeV]")))
     ###########
 
-    fill += hist.add("gen_bb", (60, -0.5, 250, ("gen_bb.mass", "gen bb mass [GeV]")))
-    fill += hist.add("genjet_from_b", (60, -0.5, 250, ("genjet_from_b.mass", "gen bb mass [GeV]")))
-    fill += hist.add("mass_reco_b_gen_match", (60, -0.5, 250, ("mass_reco_b_gen_match.mass", "gen bb mass [GeV]")))
-    fill += hist.add("bb_dr", (30, 0.5, 8, ("bb_dr", "delta r between two b-candidates")))
-    fill += hist.add("mbb", (60, -0.5, 250, ("mbb", "H-> bb candidate mass[GeV]")))
+    #fill += hist.add("gen_bb", (60, -0.5, 250, ("gen_bb.mass", "gen bb mass [GeV]")))
+    #fill += hist.add("genjet_from_b", (60, -0.5, 250, ("genjet_from_b.mass", "gen bb mass [GeV]")))
+    #fill += hist.add("mass_reco_b_gen_match", (60, -0.5, 250, ("mass_reco_b_gen_match.mass", "gen bb mass [GeV]")))
+    fill += hist.add("bb_dr", (30, -0.5, 5, ("bb_dr", r'$\Delta$ R between b-candidates')))
+    fill += hist.add("mbb", (50, -0.5, 250, ("mbb", "H-> bb candidate mass[GeV]")))
+    fill += hist.add("bjets_genjets_dr", (30, -0.5, 5, ("bjets_genjets_dr", r'$\Delta$ R between b-candidates (genjets)')))
+    fill += hist.add("bjets_genjets_mass", (50, -0.5, 250, ("bjets_genjets_mass", "H-> bb candidate (genjets) mass[GeV]")))
     fill += hist.add("nonbjet_pt", (50, -0.5, 250, ("j_nonbcand.pt", "non-bjets pT [GeV]")))
+    fill += hist.add("bjet_pt", (50, -0.5, 250, ("j_bcand.pt", "bjets pT [GeV]")))
+
     fill += hist.add("qq_mass", (50, -0.5, 250, ("qq_mass", "non-bjets pT [GeV]")))
     fill += hist.add("mT", (60, -0.5, 250, ("mT_leading_lep", "transverse mass W->lv [GeV]")))
     fill += hist.add("leading_e", (50, -0.5, 250, ("leading_e.pt", "electron pT [GeV]")))
     fill += hist.add("leading_mu", (50, -0.5, 250, ("leading_mu.pt", "muon pT [GeV]")))
+    fill += hist.add("njets", (10, -0.5, 9.5, ("njets", "jet multiplicity")))
 
     fill += hist.add("chi_sq_hadW", (30, -0.5, 6, ("chi_sq_hadW", "hadronic W region chi square")))
     fill += hist.add("chi_sq_hadWs", (30, -0.5, 6, ("chi_sq_hadWs", "leptonic W region chi square")))
     fill += hist.add("chi_sq_tt", (30, -0.5, 6, ("chi_sq_tt", "ttbar chi square")))
 
-    fill += hist.add("Hbb_vs_HWW", 
+    fill += hist.add("mbb_vs_bb_dr", 
                     (50, 0, 250, ('mbb', 'H->bb Candidate Mass [GeV]')),
-                    (50, 0, 250, ('mlvqq_hadWs', 'H->WW Candidate Mass [GeV]')))
+                    (50, 0, 5, ('bb_dr', r'$\Delta R$ between b-candidates')))
+    fill += hist.add("genjets_mbb_vs_bb_dr", 
+                    (50, 0, 250, ('bjets_genjets_mass', 'H->bb Candidate (genjets) Mass [GeV]')),
+                    (50, 0, 5, ('bjets_genjets_dr', r'$\Delta R$ between b-candidates (genjets)')))
     
-    fill += hist.add("chiSq_vs_mbb", 
-                (50, 0, 250, ('mbb','H->bb Candidate Mass [GeV]')),
-                (50, 0, 5, ('chi_sq_hadW', 'hadronic W region chi square')))
+    #fill += hist.add("Hbb_vs_HWW", 
+    #                (50, 0, 250, ('mbb', 'H->bb Candidate Mass [GeV]')),
+    #                (50, 0, 250, ('mlvqq_hadWs', 'H->WW Candidate Mass [GeV]')))
+    #
+    #fill += hist.add("chiSq_vs_mbb", 
+    #            (50, 0, 250, ('mbb','H->bb Candidate Mass [GeV]')),
+    #            (50, 0, 5, ('chi_sq_hadW', 'hadronic W region chi square')))
 
     #### THIS IS JUST A COPY OF THE PREVIOUS FILL HISTOGRAMS FUNCTION
     # fill += hist.add("hT", (50, 0, 1500, ("hT", "h_{T} [GeV]")))

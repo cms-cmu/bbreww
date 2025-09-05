bf0ea364250cc2ae62adc813c158dde1952a2796
Alejandro Gomez Espinosa <alejandro.gomez@cern.ch>
2025-09-05 13:10:01 -0400

    Add error messages for output base argument parsing in analysis scripts

--- scripts/analysis_test_mc.sh
+++ scripts/analysis_test_mc.sh
@@ -7,9 +7,10 @@ source "src/scripts/common.sh"
 
 # Parse output base argument
 OUTPUT_BASE_DIR=$(parse_output_base_arg "output/" "$@")
-# if [ $? -ne 0 ]; then
-#     exit 1
-# fi
+if [ $? -ne 0 ]; then
+    echo "Error parsing output base argument. Use --output-base DIR to specify the output directory. Default DIR=output/"
+    exit 1
+fi
 
 # Call the main analysis_test.sh script with Run3-specific parameters
 bash bbreww/scripts/run_processor.sh \

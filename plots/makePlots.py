8c795375f9587dced1d1d4ec031f3d79dadfeee8
John Alison <johnda102@gmail.com>
2025-09-05 14:02:05 -0400

    feat(plots): add interactive and batch plotting utilities for HH4b analysis
    - Introduced `iPlot.py` for interactive plotting with support for 1D/2D plots,
      variable filtering, and configuration management.
    - Added `makePlots.py` for batch processing of plots, including nominal 1D
      plots, 2D plots, and comparison plots.
    - Included metadata files (`plotModifiers.yml`, `plotsAll.yml`) for plot
      customization and configuration.
    - Added `plot_requirements.txt` to specify dependencies for the plotting
      utilities.
    - Created `analysis_plot_mc.sh` script for automated testing and generation
      of plots from metadata.

---
+++ plots/makePlots.py
@@ -0,0 +1,182 @@
+import os
+# import time
+import sys
+import yaml
+import hist
+import argparse
+import tempfile
+os.environ['MPLCONFIGDIR'] = tempfile.mkdtemp()
+import matplotlib.pyplot as plt
+from coffea.util import load
+import numpy as np
+
+sys.path.insert(0, os.getcwd())
+from src.plotting.plots import makePlot, make2DPlot, load_config_bbWW, load_hists, read_axes_and_cuts, parse_args
+import src.plotting.iPlot_config as cfg
+
+np.seterr(divide='ignore', invalid='ignore')
+
+def doPlots(varList, debug=False):
+
+    if args.doTest:
+        varList = ["qq_mass", "mbb"]
+
+    cut = "preselection"
+
+    #
+    #  Nominal 1D Plots
+    #
+    for v in varList:
+        if debug: print(f"plotting 1D ...{v}")
+
+        vDict = cfg.plotModifiers.get(v, {})
+        if debug: print(v, vDict, vDict.get("2d", False))
+        if vDict.get("2d", False):
+            continue
+
+        vDict["ylabel"] = "Entries"
+
+        vDict["legend"] = True
+        vDict["year"] = "Run3"
+        vDict["doRatio"] = False
+        #vDict["doRatio"] = cfg.plotConfig.get("doRatio", True)
+
+        if args.doTest:
+            vDict["write_yaml"] = True
+
+        for flavor in ["e", "mu", sum]:
+
+            for channel in ["hadronic_W", "leptonic_W", sum]:
+
+                if debug: print(f"plotting 1D ...{v}")
+                plot_args  = {}
+                plot_args["var"] = v
+                plot_args["cut"] = cut
+                plot_args["outputFolder"] = args.outputFolder
+                plot_args["axis_opts"] = {"flavor":flavor, "channel":channel}
+                plot_args = plot_args | vDict
+                if debug: print(plot_args)
+                try:
+                    fig = makePlot(cfg, **plot_args)
+                except ValueError:
+                    print(f"ValueError: {v} {region}")
+                    pass
+
+                plt.close()
+
+#    #
+#    #  2D Plots
+#    #
+#    for v in varList:
+#        print(v)
+#
+#        vDict = cfg.plotModifiers.get(v, {})
+#
+#        if not vDict.get("2d", False):
+#            continue
+#
+#        vDict["ylabel"] = "Entries"
+#        vDict["doRatio"] = cfg.plotConfig.get("doRatio", True)
+#        vDict["legend"] = True
+#
+#        if args.doTest:
+#            vDict["write_yaml"] = True
+#
+#        for process in ["data", "Multijet", "HH4b", "TTbar"]:
+#            for region in ["SR", "SB"]:
+#
+#                plot_args  = {}
+#                plot_args["var"] = v
+#                plot_args["cut"] = cut
+#                plot_args["region"] = region
+#                plot_args["outputFolder"] = args.outputFolder
+#                plot_args = plot_args | vDict
+#
+#                if debug: print("process is ",process)
+#                if debug: print(plot_args)
+#
+#                fig = make2DPlot(cfg, process,
+#                                 **plot_args)
+#                plt.close()
+#
+#    #
+#    #  Comparison Plots
+#    #
+#    varListComp = []
+#    if args.doTest:
+#        varListComp = ["v4j.mass", "SvB_MA.ps", "quadJet_selected.xHH"]
+#
+#        for v in varListComp:
+#            print(v)
+#
+#            vDict = cfg.plotModifiers.get(v, {})
+#
+#            vDict["ylabel"] = "Entries"
+#            vDict["doRatio"] = cfg.plotConfig.get("doRatio", True)
+#            vDict["legend"] = True
+#
+#            if args.doTest:
+#                vDict["write_yaml"] = True
+#
+#            for process in ["data", "Multijet", "HH4b", "TTbar"]:
+#
+#                #
+#                # Comp Cuts
+#                #
+#                for region in ["SR", "SB"]:
+#
+#                    plot_args  = {}
+#                    plot_args["var"] = v
+#                    plot_args["cut"] = ["passPreSel", "failSvB", "passSvB"]
+#                    plot_args["region"] = region
+#                    plot_args["outputFolder"] = args.outputFolder
+#                    plot_args["process"] = process
+#                    plot_args["norm"] = True
+#                    plot_args = plot_args | vDict
+#
+#                    if debug: print(plot_args)
+#
+#                    fig = makePlot(cfg, **plot_args)
+#
+#
+#
+#                    plt.close()
+#
+#                #
+#                # Comp Regions
+#                #
+#                fig = makePlot(cfg,
+#                               var=v,
+#                               cut="passPreSel",
+#                               region=["SR", "SB"],
+#                               process=process,
+#                               outputFolder=args.outputFolder,
+#                               **vDict
+#                               )
+#
+#                plt.close()
+
+
+if __name__ == '__main__':
+
+    args = parse_args()
+
+    cfg.plotConfig = load_config_bbWW(args.metadata)
+    cfg.outputFolder = args.outputFolder
+
+    cfg.plotModifiers = yaml.safe_load(open(args.modifiers, 'r'))
+
+    if cfg.outputFolder:
+        if not os.path.exists(cfg.outputFolder):
+            os.makedirs(cfg.outputFolder)
+
+    cfg.hists = load_hists(args.inputFile)
+    cfg.fileLabels = args.fileLabels
+    cfg.axisLabels, cfg.cutList = read_axes_and_cuts(cfg.hists, cfg.plotConfig)
+
+    if args.list_of_hists:
+        varList = args.list_of_hists
+    else:
+        varList = [h for h in cfg.hists[0]['hists'].keys() if not any(skip in h for skip in args.skip_hists)]
+
+    doPlots(varList, debug=args.debug)
---
+++ scripts/analysis_plot_mc.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+# Source common functions
+source "src/scripts/common.sh"
+
+
+INPUT_DIR="${1:-"output"}/analysis_test_mc"
+OUTPUT_DIR="${1:-"output"}/analysis_plot_mc"
+echo "############### Checking and creating output directory"
+if [ ! -d $OUTPUT_DIR ]; then
+    mkdir -p $OUTPUT_DIR
+fi
+
+echo "############### Running test processor"
+python bbreww/plots/makePlots.py $INPUT_DIR/test.coffea --doTest -o $OUTPUT_DIR -m bbreww/plots/metadata/plotsAll.yml --modifiers bbreww/plots/metadata/plotModifiers.yml
+
+
+echo "############### Checking if pdf files exist"
+ls $OUTPUT_DIR/Run3/preselection/flavor_sum/channel_sum/mbb.pdf
+ls $OUTPUT_DIR/Run3/preselection/flavor_e/channel_sum/mbb.pdf
+ls $OUTPUT_DIR/Run3/preselection/flavor_mu/channel_sum/mbb.pdf
+#ls $OUTPUT_DIR/RunII/passPreSel/fourTag/SR/SvB_MA_ps_hh.pdf
+#ls $OUTPUT_DIR/RunII/passPreSel/fourTag/SR_vs_SB/data/SvB_MA_ps.pdf
+#ls $OUTPUT_DIR/RunII/passPreSel/fourTag/SR_vs_SB/HH4b/SvB_MA_ps.pdf
+#ls $OUTPUT_DIR/RunII/passPreSel_vs_failSvB_vs_passSvB/fourTag/SR/data/v4j_mass.pdf
+#ls $OUTPUT_DIR/RunII/passPreSel_vs_failSvB_vs_passSvB/fourTag/SR/HH4b/v4j_mass.pdf 
+#ls $OUTPUT_DIR/RunII/passPreSel/fourTag/SR/data/quadJet_min_dr_close_vs_other_m.pdf 
+#ls $OUTPUT_DIR/RunII/passPreSel/fourTag/SR/HH4b/quadJet_min_dr_close_vs_other_m.pdf
+#ls $OUTPUT_DIR/RunII/passPreSel/threeTag/SR/Multijet/quadJet_min_dr_close_vs_other_m.pdf 
+
+
+echo "############### check making the plots from yaml "
+python src/plotting/plot_from_yaml.py --input_yaml \
+       $OUTPUT_DIR/Run3/preselection/flavor_sum/channel_sum/mbb.yaml \
+       $OUTPUT_DIR/Run3/preselection/flavor_e/channel_sum/mbb.yaml \
+       $OUTPUT_DIR/Run3/preselection/flavor_mu/channel_sum/mbb.yaml \
+       --out $OUTPUT_DIR/test_plots_from_yaml 
+ 
+echo "############### Checking if pdf files exist"
+ls $OUTPUT_DIR/test_plots_from_yaml/Run3/preselection/flavor_sum/channel_sum/mbb.pdf
+ls $OUTPUT_DIR/test_plots_from_yaml/Run3/preselection/flavor_e/channel_sum/mbb.pdf
+ls $OUTPUT_DIR/test_plots_from_yaml/Run3/preselection/flavor_mu/channel_sum/mbb.pdf
+# ls $OUTPUT_DIR/test_plots_from_yaml/RunII/passPreSel/fourTag/SR/SvB_MA_ps_zh.pdf
+# ls $OUTPUT_DIR/test_plots_from_yaml/RunII/passPreSel/fourTag/SR/SvB_MA_ps_hh.pdf
+# ls $OUTPUT_DIR/test_plots_from_yaml/RunII/passPreSel/fourTag/SR_vs_SB/data/SvB_MA_ps.pdf
+# ls $OUTPUT_DIR/test_plots_from_yaml/RunII/passPreSel/fourTag/SR_vs_SB/HH4b/SvB_MA_ps.pdf
+# ls $OUTPUT_DIR/test_plots_from_yaml/RunII/passPreSel_vs_failSvB_vs_passSvB/fourTag/SR/data/v4j_mass.pdf
+# ls $OUTPUT_DIR/test_plots_from_yaml/RunII/passPreSel_vs_failSvB_vs_passSvB/fourTag/SR/HH4b/v4j_mass.pdf 
+# ls $OUTPUT_DIR/test_plots_from_yaml/RunII/passPreSel/fourTag/SR/data/quadJet_min_dr_close_vs_other_m.pdf 
+# ls $OUTPUT_DIR/test_plots_from_yaml/RunII/passPreSel/fourTag/SR/HH4b/quadJet_min_dr_close_vs_other_m.pdf
+# ls $OUTPUT_DIR/test_plots_from_yaml/RunII/passPreSel/threeTag/SR/Multijet/quadJet_min_dr_close_vs_other_m.pdf 
+

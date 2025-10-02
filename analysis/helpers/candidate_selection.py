ff93204a6ef5b313e8a3746dda0a5406c8bbefa5
Aniket Khanal <akhanal@cmslpc330.fnal.gov>
2025-10-02 15:56:15 -0500

    update candidate selection

--- analysis/helpers/candidate_selection.py
+++ analysis/helpers/candidate_selection.py
@@ -32,35 +32,7 @@ def candidate_selection(events, params, year):
     Wqq_cand["dr"]   = Wqq_cand["lead"].delta_r  (Wqq_cand["subl"])
     Wqq_cand["dphi"] = Wqq_cand["lead"].delta_phi(Wqq_cand["subl"])
 
-    j_candidates = events.j_init[ak.argsort(events.j_init.pt, axis=1, ascending=False)] # pt sort to take higher pT when b-tag scores are tied
-    j_candidates = j_candidates[ak.argsort(getattr(j_candidates,bTag_key), axis=1, ascending=False)]#particleNetAK4_B btagPNetB
-    
-    j_bcand_pool = j_candidates[j_candidates.pt > 25.0]  # Only jets > 25 GeV for b-jets
-    j_bcand_pool = j_bcand_pool[getattr(j_bcand_pool,bTag_key) > btag_threshold]
-
-    # bjets requirement
-    events['has_2_bjets'] = ak.num(j_bcand_pool, axis=1) >= 2
-    events['has_1_bjet'] = ak.num(j_bcand_pool, axis=1) >= 1 #add for cutflow plot
-
-    # Mask the entire event if not enough jets or b-jets   
-    j_candidates = ak.mask(j_candidates, events.has_2_bjets)
-    j_bcand_pool = ak.mask(j_bcand_pool, events.has_2_bjets)
-    events['j_bcand'] = j_bcand_pool[:,:2]
-    j_bcand_sorted = events.j_bcand[ak.argsort(events.j_bcand.pt, axis=1, ascending=False)]
-    events['j_bcand_lead'] = j_bcand_sorted[:,0]
-    events['j_bcand_sublead'] = j_bcand_sorted[:,1] 
-
-    # nominal non-bjet selection
-    j_candidates_nom = j_candidates[j_candidates.isnominal] # pt > 25 GeV jets (nominal)
-    j_candidates_nom = j_candidates_nom[:,2:] # pick other jets after taking two b-jets
-    j_candidates_nom = j_candidates_nom[ak.argsort(j_candidates_nom.pt, axis=1, ascending=False)] # pT sort the jets
-    
-    j_candidates_nom = ak.mask(j_candidates_nom, ak.num(j_candidates_nom,axis=1)>=2) # require 2 or more non-bjets
-    j_candidates_nom = j_candidates_nom[:,:2] # take leading two pT jets
-    events['j_nonbcand_nom'] = j_candidates_nom
-    events['j_nonbcand_nom_lead_pt'] =    ak.fill_none(j_candidates_nom[:,0].pt, np.nan, axis=0)
-    events['j_nonbcand_nom_sublead_pt'] = ak.fill_none(j_candidates_nom[:,1].pt, np.nan, axis=0)
-    events['qq_nom'] = j_candidates_nom[:,0] + j_candidates_nom[:,1]
+    events['Wqq_cand'] = Wqq_cand
 
     #
     # soft jets analysis
@@ -77,9 +49,12 @@ def candidate_selection(events, params, year):
     #jj_i = jj_i[(q_cands_soft[jj_i.j1] + q_cands_soft[jj_i.j2]).mass<120.0] #dijet cuts
     events['dijet_combs_new'] = jj_i
 
-    events['njets'] = ak.fill_none(ak.num(j_clean[j_clean.isnominal],axis=1),np.nan)
-    events['HT'] = ak.sum(events.j_init.pt, axis=1)
-    
+    events['j_lead_new'] =  q_cands_soft[jj_i.j1] # leading jet
+    events['j_sublead_new'] =  q_cands_soft[jj_i.j2] # subleading jet
+
+    events['qq_mass'] = ak.fill_none((q_cands_soft[jj_i.j1] + q_cands_soft[jj_i.j2]).mass,np.nan) # plotting gives issues with None values
+    events['qq_soft'] = ak.pad_none(q_cands_soft[jj_i.j1] + q_cands_soft[jj_i.j2], 3, axis=1)
+
     return events
 
 ## function only for skimmer

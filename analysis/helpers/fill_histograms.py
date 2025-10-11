187431c89830c4679d5dac08d9567c96948eb3c4
John Alison <johnalison@cmu.edu>
2025-10-10 19:44:32 -0500

    Cleaning up chi2

--- analysis/helpers/fill_histograms.py
+++ analysis/helpers/fill_histograms.py
@@ -12,6 +12,11 @@ def add_bbWW_common_hists(fill, hist):
     fill += hist.add("MET", (50, -0.5, 250, ("MET.pt", "MET pT [GeV]")))
     fill += hist.add("njets", (10, -0.5, 9.5, ("njets", "jet multiplicity")))
 
+    fill += hist.add("chi_sq_hadW",  (30, -0.5, 6, ("chi_sq_hadW",  "hadronic W region chi square")))
+    fill += hist.add("chi_sq_hadWs", (30, -0.5, 6, ("chi_sq_hadWs", "leptonic W region chi square")))
+    fill += hist.add("chi_sq_tt",    (30, -0.5, 6, ("chi_sq_tt",    "ttbar chi square")))
+
+
     #
     # Hbb Candidate
     #
@@ -80,28 +85,16 @@ def fill_histograms_nominal(
     fill += LorentzVector.plot_pair( ("HWW", R"$H_{WW}$"), "Hww_cand", skip=["n","lead","subl","st"], bins={"mass": (100, 100, 400)}, )
 
 
-    fill += hist.add("bjets_genjets_dr", (30, -0.5, 5, ("bjets_genjets_dr", r'$\Delta$ R between b-candidates (genjets)')))
+    fill += hist.add("bjets_genjets_dr",   (30, -0.5, 5, ("bjets_genjets_dr", r'$\Delta$ R between b-candidates (genjets)')))
     fill += hist.add("bjets_genjets_mass", (50, -0.5, 250, ("bjets_genjets_mass", "H-> bb candidate (genjets) mass[GeV]")))
 
-    fill += hist.add("chi_sq_hadW", (30, -0.5, 6, ("chi_sq_hadW", "hadronic W region chi square")))
-    fill += hist.add("chi_sq_hadWs", (30, -0.5, 6, ("chi_sq_hadWs", "leptonic W region chi square")))
-    fill += hist.add("chi_sq_tt", (30, -0.5, 6, ("chi_sq_tt", "ttbar chi square")))
-
     fill += hist.add("genjets_mbb_vs_bb_dr",
-                    (50, 0, 250, ('bjets_genjets_mass', 'H->bb Candidate (genjets) Mass [GeV]')),
-                    (50, 0, 5, ('bjets_genjets_dr', r'$\Delta R$ between b-candidates (genjets)')))
+                     (50, 0, 250, ('bjets_genjets_mass', 'H->bb Candidate (genjets) Mass [GeV]')),
+                     (50, 0, 5, ('bjets_genjets_dr', r'$\Delta R$ between b-candidates (genjets)')))
 
     fill += hist.add("lep_qq_pt_dr",
                 (50, 0, 250, ('leading_lep.pt', 'leading lepton pT [GeV]')),
                 (50, 0, 5, ('lep_qq_dr', r'$\Delta R$ between leading lepton and selected qq')))
-
-    #fill += hist.add("Hbb_vs_HWW",
-    #                (50, 0, 250, ('mbb', 'H->bb Candidate Mass [GeV]')),
-    #                (50, 0, 250, ('mlvqq_hadWs', 'H->WW Candidate Mass [GeV]')))
-    #
-    #fill += hist.add("chiSq_vs_mbb",
-    #            (50, 0, 250, ('mbb','H->bb Candidate Mass [GeV]')),
-    #            (50, 0, 5, ('chi_sq_hadW', 'hadronic W region chi square')))
 
 
     # fill histograms
@@ -140,19 +133,14 @@ def fill_histograms(
     fill, hist = add_bbWW_common_hists(fill, hist)
 
 
-    #fill += hist.add("gen_bb", (60, -0.5, 250, ("gen_bb.mass", "gen bb mass [GeV]")))
-    #fill += hist.add("genjet_from_b", (60, -0.5, 250, ("genjet_from_b.mass", "gen bb mass [GeV]")))
-    #fill += hist.add("mass_reco_b_gen_match", (60, -0.5, 250, ("mass_reco_b_gen_match.mass", "gen bb mass [GeV]")))
-    fill += hist.add("bjets_genjets_dr", (30, -0.5, 5, ("bjets_genjets_dr", r'$\Delta$ R between b-candidates (genjets)')))
+    fill += hist.add("bjets_genjets_dr",   (30, -0.5, 5, ("bjets_genjets_dr", r'$\Delta$ R between b-candidates (genjets)')))
     fill += hist.add("bjets_genjets_mass", (50, -0.5, 250, ("bjets_genjets_mass", "H-> bb candidate (genjets) mass[GeV]")))
 
-    fill += hist.add("chi_sq_hadW", (30, -0.5, 6, ("chi_sq_hadW", "hadronic W region chi square")))
-    fill += hist.add("chi_sq_hadWs", (30, -0.5, 6, ("chi_sq_hadWs", "leptonic W region chi square")))
-    fill += hist.add("chi_sq_tt", (30, -0.5, 6, ("chi_sq_tt", "ttbar chi square")))
 
     fill += hist.add("genjets_mbb_vs_bb_dr",
                     (50, 0, 250, ('bjets_genjets_mass', 'H->bb Candidate (genjets) Mass [GeV]')),
                     (50, 0, 5, ('bjets_genjets_dr', r'$\Delta R$ between b-candidates (genjets)')))
+
     fill += hist.add("lep_qq_pt_dr",
                 (50, 0, 250, ('leading_lep.pt', 'leading lepton pT [GeV]')),
                 (50, 0, 5, ('lep_qq_dr', r'$\Delta R$ between leading lepton and selected qq')))

3eaa80f563f75ddaa3af69154f6e22d687e0efbd
Aniket Khanal <akhanal@cmslpc311.fnal.gov>
2025-11-18 14:04:05 -0600

    revert changes to test script

--- scripts/analysis_plot_mc.sh
+++ scripts/analysis_plot_mc.sh
@@ -9,7 +9,7 @@ if [ $? -ne 0 ]; then
 fi
 
 
-INPUT_DIR="${OUTPUT_BASE_DIR}/full_run"
+INPUT_DIR="${OUTPUT_BASE_DIR}/analysis_test_mc"
 OUTPUT_DIR="${OUTPUT_BASE_DIR}/analysis_plot_mc"
 echo "############### Checking and creating output directory"
 if [ ! -d $OUTPUT_DIR ]; then
@@ -18,11 +18,13 @@ fi
 
 echo "############### ls input file"
 ls $INPUT_DIR/
-ls $INPUT_DIR/output.coffea
+ls $INPUT_DIR/test.coffea
 
 display_section_header "Running makePlots.py"
-run_command python bbreww/plots/makePlots.py $INPUT_DIR/output.coffea -o $OUTPUT_DIR -m bbreww/plots/metadata/plotsAll.yml --modifiers bbreww/plots/metadata/plotModifiers.yml
+run_command python bbreww/plots/makePlots.py $INPUT_DIR/test.coffea --doTest -o $OUTPUT_DIR -m bbreww/plots/metadata/plotsAll.yml --modifiers bbreww/plots/metadata/plotModifiers.yml
 
+### run this line to run locally for all plots
+# python bbreww/plots/makePlots.py output/full_run/output.coffea -o output/analysis_plot_mc -m bbreww/plots/metadata/plotsAll.yml --modifiers bbreww/plots/metadata/plotModifiers.yml
 
 display_section_header "Checking if pdf files exist"
 ls $OUTPUT_DIR/Run3/preselection/flavor_sum/channel_sum/Hbb_mass.pdf

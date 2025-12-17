0820e4083f8959047b1050eb7e2e5a58b7db8a4e
Aniket Khanal <akhanal@cmslpc332.fnal.gov>
2025-12-17 10:36:02 -0600

    add new histograms

--- analysis/helpers/fill_histograms.py
+++ analysis/helpers/fill_histograms.py
@@ -21,7 +21,6 @@ def add_bbWW_common_hists(fill, hist):
                     (50, 0, 250, ('Hbb_cand.mass', 'H->bb Candidate Mass [GeV]')),
                     (50, 0,   5, ('Hbb_cand.dr', r'$\Delta R$ between b-candidates')))
 
-
     #
     # Wlnu Candidate
     #
@@ -34,7 +33,6 @@ def add_bbWW_common_hists(fill, hist):
     fill += Elec.plot( ("Elec", R"$Elec$"), "sel_elec", skip=["n"], )
     fill += Muon.plot( ("Muon", R"$Muon$"), "sel_muon", skip=["n"], )
 
-
     #
     #  From before
     #
@@ -46,11 +44,17 @@ def add_bbWW_common_hists(fill, hist):
                     (50, 0, 250, ('bjets_genjets_mass', 'H->bb Candidate (genjets) Mass [GeV]')),
                     (50, 0, 5, ('bjets_genjets_dr', r'$\Delta R$ between b-candidates (genjets)')))
 
-    fill += hist.add("lep_qq_pt_dr",
-                (50, 0, 250, ('leading_lep.pt', 'leading lepton pT [GeV]')),
-                (50, 0, 5, ('lep_qq_dr', r'$\Delta R$ between leading lepton and selected qq')))
+    fill += hist.add("genW_mass_vs_subl_jet_pt",
+                (50, 0, 250, ('gen_hadW.mass', 'W->qq gen mass [GeV]')),
+                (50, 0, 250, ('true_ak4_2', r'W->qq subleading jet $p_T$')))
 
-
+    # jet selection efficiencies
+    fill += hist.add("true_jets_sublead.pt", (50, -0.5, 250, ("true_ak4_2", "pT [GeV]")))
+    fill += hist.add("true_jets_lead.pt", (50, -0.5, 250, ("true_ak4_1", "pT [GeV]")))
+    fill += hist.add("true_soft_jets_sel_sublead.pt", (50, -0.5, 250, ("q_soft_true_sublead", "pT[GeV]"))) # softjet 1
+    fill += hist.add("true_soft_jets_sel_lead.pt", (50, -0.5, 250, ("q_soft_true_lead", "pT[GeV]"))) # softjet 2
+    fill += hist.add("true_nom_jets_sel_sublead.pt", (50, -0.5, 250, ("q_nom_true_sublead", "pT[GeV]"))) # nominal jet 1
+    fill += hist.add("true_nom_jets_sel_lead.pt", (50, -0.5, 250, ("q_nom_true_lead", "pT[GeV]"))) # nominal jet 2
 
     return fill, hist
 
@@ -63,7 +67,7 @@ def fill_histograms_nominal(
     histCuts: list = ['preselection'],
     channel_list: list = ['hadronic_W','leptonic_W'],
     flavor_list: list = ['e', 'mu'],
-    region_list: list = ['SR', 'CR'],
+    #region_list: list = ['SR', 'CR'],
     run_SvB: bool = False
 ):
 
@@ -77,7 +81,7 @@ def fill_histograms_nominal(
         year=[year],
         channel=channel_list,
         flavor = flavor_list,
-        region = region_list,
+        #region = region_list,
         **dict((s, ...) for s in histCuts)
     )
 
@@ -102,6 +106,9 @@ def fill_histograms_nominal(
     fill += LorentzVector.plot_pair( ("HWW", R"$H_{WW}$"), "Hww_cand", skip=["n","lead","subl","st"], bins={"mass": (100, 100, 400)}, )
     fill += hist.add("HWW.lqq_dr", (50, -0.5, 10, ("Hww_cand.lqq_dr", "qq - lepton  delta R")))
 
+    fill += hist.add("mbb_vs_lep_qq_dr",
+            (50, 0, 250, ('Hbb_cand.mass', 'H->bb Candidate Mass [GeV]')),
+            (50, 0, 5, ('Hww_cand.lqq_dr', r'$\Delta R$ between leading lepton and selected qq')))
     #
     #  TTbar Candidate
     #
@@ -125,7 +132,7 @@ def fill_histograms(
     histCuts: list = ['preselection'],
     channel_list: list = ['hadronic_W','leptonic_W'],
     flavor_list: list = ['e', 'mu'],
-    region_list: list = ['SR', 'CR']
+    #region_list: list = ['SR', 'CR']
 ):
 
     fill = Fill(
@@ -138,7 +145,7 @@ def fill_histograms(
         year=[year],
         channel=channel_list,
         flavor = flavor_list,
-        region = region_list,
+        #region = region_list,
         **dict((s, ...) for s in histCuts)
     )
 

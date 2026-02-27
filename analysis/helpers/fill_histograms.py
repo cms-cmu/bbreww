7808ac1705e73498f9c057c3569cf49732a7ee80
Aniket Khanal <akhanal@cmslpc307.fnal.gov>
2026-02-27 14:38:06 -0600

    add regression target histograms

--- analysis/helpers/fill_histograms.py
+++ analysis/helpers/fill_histograms.py
@@ -14,6 +14,7 @@ def add_bbWW_common_hists(fill, hist, SvB: bool = False, MET_regression: bool = 
     fill += hist.add("MET_pt", (50, -0.5, 250, ("MET.pt", R"MET $p_T$ [GeV]")))
     fill += hist.add("MET_phi", (50, -4, 4, ("MET.phi", R"MET $Phi$")))
     fill += hist.add("njets", (10, -0.5, 9.5, ("njets", "jet multiplicity")))
+    fill += hist.add("lepnu_deta", (50, -3, 3, ("Wlnu_cand.deta", "delta_eta between lepton and neutrino")))
 
     ## W-qq quark vs. gluon selection candidates
     fill += hist.add("qvgScore", (50, 0, 1.0, ("q_cands_soft.btagPNetQvG", "ak4 jets quark vs. gluon score")))
@@ -25,6 +26,12 @@ def add_bbWW_common_hists(fill, hist, SvB: bool = False, MET_regression: bool = 
     fill += hist.add("mbb_vs_bb_dr",
                     (50, 0, 250, ('Hbb_cand.mass', 'H->bb Candidate Mass [GeV]')),
                     (50, 0,   5, ('Hbb_cand.dr', r'$\Delta R$ between b-candidates')))
+
+    fill += hist.add("mbb_vs_mqq",
+                    (50, 0, 250, ('Hbb_cand.mass', 'H->bb Candidate Mass [GeV]')),
+                     (30, 0, 150, ('mqq', r'$\Delta R$ between b-candidates')),
+                     mqq= lambda events: ak.fill_none((events.sel_qq_l + events.sel_qq_sl).mass, np.nan)
+                     )
 
     #
     # Wlnu Candidate and reconstructed neutrino pz
@@ -63,23 +70,22 @@ def add_bbWW_common_hists(fill, hist, SvB: bool = False, MET_regression: bool = 
     if MET_regression:
         fill += hist.add("genNu_pt", (50, 0, 200, ("genNu_pt", R"gen $\nu p_T$ [GeV]")))
         fill += hist.add("genNu_eta", (50, -5, 5, ("genNu_eta", R"gen $\nu \eta$")))
-        fill += hist.add("genNu_phi", (50, -5, 5, ("genNu_phi", R"gen $\nu \phi$")))
+        fill += hist.add("genNu_pz", (80, -200, 200, ("genNu_pz", R"gen $\nu p_z$")))
         fill += hist.add("gen_lepW_mass", (30, 0, 150, ("gen_lepW_mass", R"gen leptonic $m_W$ [GeV]")))
         
         fill += hist.add("islepW", (2, 0, 1.1, ("isLepW", R"leptonic W on shell boolean")))
 
         fill += hist.add("reg_mW",
                           (30, 0, 150, ('reg_mW', R"Regressed leptonic W mass [GeV]")),
-                          reg_mW=lambda events: ak.fill_none((events.reg_nu + events.leading_lep).mass, np.nan)
-                          )
+                          reg_mW=lambda events: ak.fill_none((events.reg_nu + events.leading_lep).mass, np.nan))
 
         fill += hist.add("reg_nu_pt_res",
-                        (60, -100, 100, ("nu_pt_res", R"(True - Regressed) $\nu p_T$ [GeV]")),
+                        (40, -100, 100, ("nu_pt_res", R"(True - Regressed) $\nu p_T$ [GeV]")),
                         nu_pt_res=lambda events: events.genNu_pt - events.reg_nu.pt
                         )
-        fill += hist.add("reg_nu_phi_res",
-                        (50, -5, 5, ("nu_phi_res", R"(True - Regressed) $\nu \Phi$")),
-                        nu_phi_res=lambda events: events.genNu_phi -events.reg_nu.phi
+        fill += hist.add("reg_nu_pz_res",
+                        (40, -100, 100, ("nu_pz_res", R"(True - Regressed) $\nu |p_z|$")),
+                        nu_pz_res=lambda events: abs(events.genNu_pz) - abs(events.reg_nu.pz)
                         )
         fill += hist.add("reg_nu_eta_res",
                         (50, -5, 5, ("nu_eta_res", R"(True - Regressed) $\nu \eta$")),
@@ -100,11 +106,12 @@ def add_bbWW_common_hists(fill, hist, SvB: bool = False, MET_regression: bool = 
                          )
         
         fill += regressionHists(("met_regressor", "MET Regressor"), "met_regressor")
+        fill += hist.add("HWW_mass", (50, 0, 250, ("HWW_mass", "H-> WW mass [GeV]")))
 
-#        fill += hist.add("mbb_vs_mWW",
-#                         (50, 0, 250, ('Hbb_cand.mass', 'H->bb Candidate Mass [GeV]')),
-#                         (50, 0,   5, ('HWW', r'$\Delta R$ between b-candidates')),
-#                         HWW= lambda events: (events.reg_nu + events.leading_lep + events.q_cands_nom[:,0] + events.q_cands_nom[:,1]).mass)
+        
+        fill += hist.add("mbb_vs_mWW",
+                         (50, 0, 250, ('Hbb_cand.mass', 'H->bb Candidate Mass [GeV]')),
+                         (50, 0, 250, ('HWW_mass', r'$\Delta R$ between b-candidates')))
 
     return fill, hist
 
@@ -149,9 +156,15 @@ def fill_histograms_nominal(
     fill += hist.add("true_soft_jets_sel_lead.pt", (50, -0.5, 250, ("q_soft_true_lead", "pT[GeV]"))) # softjet 2
     fill += hist.add("true_nom_jets_sel_sublead.pt", (50, -0.5, 250, ("q_nom_true_sublead", "pT[GeV]"))) # nominal jet 1
     fill += hist.add("true_nom_jets_sel_lead.pt", (50, -0.5, 250, ("q_nom_true_lead", "pT[GeV]"))) # nominal jet 2    
-    fill += hist.add("true_ml_jets_sel_lead.pt", (50, -0.5, 250, ("q_ml_true_lead", "pT[GeV]"))) # ml classifier jet 1
-    fill += hist.add("true_ml_jets_sel_sublead.pt", (50, -0.5, 250, ("q_ml_true_sublead", "pT[GeV]"))) # ml classifier jet 2        
-    
+    fill += hist.add("true_ml_jets_sel_lead.pt", (50, -0.5, 250, ("q_ml_true_lead", "pT[GeV]"))) # ml both correct
+    fill += hist.add("true_ml_jets_sel_sublead.pt", (50, -0.5, 250, ("q_ml_true_sublead", "pT[GeV]"))) # ml both correct
+    fill += hist.add("ml_lead_denom.pt", (50, -0.5, 250, ("q_ml_lead_denom", "pT[GeV]"))) # >= 1 true jet
+    fill += hist.add("ml_lead_numer.pt", (50, -0.5, 250, ("q_ml_lead_numer", "pT[GeV]"))) # ML lead correct
+    fill += hist.add("ml_sublead_denom.pt", (50, -0.5, 250, ("q_ml_sublead_denom", "pT[GeV]"))) # >= 2 true jets
+    fill += hist.add("ml_sublead_numer.pt", (50, -0.5, 250, ("q_ml_sublead_numer", "pT[GeV]"))) # ML sublead correct
+    fill += hist.add("misclass_p_onshell", (10, 0, 1.1, ("misclass_p_onshell", "p(on-shell)"))) # misclassified & gen_lepW < 20
+    fill += hist.add("misclass_sigma_pz_on", (100, 0, 300, ("misclass_sigma_pz_on", "on-shell uncertainty"))) # misclassified & gen_lepW < 20
+
     fill += Chi2Hists(("chi2_hadWs",      "chi2 hadWs"),         "chi2_hadWs")
     fill += Chi2Hists(("chi2_hadW",       "chi2 hadW"),          "chi2_hadW")
     fill += Chi2Hists(("chi2_tt",         "chi2 tt"),            "chi2_tt")
@@ -213,9 +226,15 @@ def fill_histograms(
     fill += hist.add("true_soft_jets_sel_lead.pt", (10, 14.5, 30, ("q_soft_true_lead", "pT[GeV]"))) # softjet 2
     fill += hist.add("true_nom_jets_sel_sublead.pt", (10, 14.5, 30, ("q_nom_true_sublead", "pT[GeV]"))) # nominal jet 1
     fill += hist.add("true_nom_jets_sel_lead.pt", (10, 14.5, 30, ("q_nom_true_lead", "pT[GeV]"))) # nominal jet 2
-    fill += hist.add("true_ml_jets_sel_lead.pt", (10, 14.5, 30, ("q_ml_true_lead", "pT[GeV]"))) # ml classifier jet 1
-    fill += hist.add("true_ml_jets_sel_sublead.pt", (10, 14.5, 30, ("q_ml_true_sublead", "pT[GeV]"))) # ml classifier jet 2        
-    
+    fill += hist.add("true_ml_jets_sel_lead.pt", (10, 14.5, 30, ("q_ml_true_lead", "pT[GeV]"))) # ml both correct
+    fill += hist.add("true_ml_jets_sel_sublead.pt", (10, 14.5, 30, ("q_ml_true_sublead", "pT[GeV]"))) # ml both correct
+    fill += hist.add("ml_lead_denom.pt", (10, 14.5, 30, ("q_ml_lead_denom", "pT[GeV]"))) # >= 1 true jet
+    fill += hist.add("ml_lead_numer.pt", (10, 14.5, 30, ("q_ml_lead_numer", "pT[GeV]"))) # ML lead correct
+    fill += hist.add("ml_sublead_denom.pt", (10, 14.5, 30, ("q_ml_sublead_denom", "pT[GeV]"))) # >= 2 true jets
+    fill += hist.add("ml_sublead_numer.pt", (10, 14.5, 30, ("q_ml_sublead_numer", "pT[GeV]"))) # ML sublead correct
+    fill += hist.add("misclass_p_onshell", (10, 0, 1.1, ("misclass_p_onshell", "p(on-shell)"))) # misclassified & gen_lepW < 20
+    fill += hist.add("misclass_sigma_pz_on", (50, 0, 100, ("misclass_sigma_pz_on", "on-shell uncertainty"))) # misclassified & gen_lepW < 20
+
     fill += Chi2Hists(("chi2_hadWs", "chi2 hadWs"), "chi2_hadWs",
                       skip=["tot_4j", "Hww_mass", "Wqq_mass",]
                       )

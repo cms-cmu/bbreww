90f801033b2f65b53a113a61de9424fa9087a1b3
Aniket Khanal <akhanal@cmslpc368.fnal.gov>
2026-03-23 16:36:49 -0500

    fix double filtering no-op bug

--- analysis/helpers/object_selection.py
+++ analysis/helpers/object_selection.py
@@ -61,10 +61,10 @@ def jet_selection(events, params, year):
     events['nsoftjets']= ak.num(j_soft, axis=1)
     events['njets'] = ak.fill_none(ak.num(j_clean[j_clean.isnominal],axis=1),np.nan)
     #events['njets'] = ak.fill_none(ak.num(j_init,axis=1),np.nan) # all jets, including soft
-    events['has_3_presel_jets'] = (ak.num(j_init[j_init.preselected],axis=1)>2)
-    events['has_exactly_3_presel_jets'] = (ak.num(j_init[j_init.preselected],axis=1)==3)
-    events['has_4_presel_jets'] = (ak.num(j_init[j_init.preselected],axis=1)>3)
-
+    events['has_3_presel_jets'] = (ak.num(j_init, axis=1) > 2)
+    events['has_exactly_3_presel_jets'] = (ak.num(j_init, axis=1) == 3)
+    events['has_4_presel_jets'] = (ak.num(j_init, axis=1) > 3)
+    
     events['HT'] = ak.sum(j_init.pt, axis=1) # HT of all jets
     events['HTsoft'] = ak.sum(j_soft.pt, axis=1) # HT of soft jets
 
@@ -80,7 +80,7 @@ def jet_selection(events, params, year):
     j_candidates_nom = j_candidates[j_candidates.isnominal]
     events['nom_njets4'] = (ak.num(j_candidates_nom, axis=1) > 3)
     events['nom_njets3'] = (ak.num(j_candidates_nom, axis=1) == 3)
-
+    
     j_btagged = j_candidates_nom[getattr(j_candidates_nom,bTag_key) > btag_threshold]
     b_cands = j_btagged[:,:2]
     b_cands = b_cands[ak.argsort(b_cands.pt, axis=1, ascending=False)] #particleNetAK4_B btagPNetB
@@ -103,11 +103,11 @@ def jet_selection(events, params, year):
     #
     j_candidates_soft = j_candidates[j_candidates.issoft]
     events['j_soft'] = j_candidates_soft
-
+    
     q_cands_soft = ak.concatenate([q_cands_nom, j_candidates_soft], axis=1)
     q_cands_soft = q_cands_soft[ak.argsort(q_cands_soft.pt, axis=1, ascending=False)] # pT sort the jets
     events['q_cands_soft_init'] = q_cands_soft
-
+    print(events.q_cands_soft_init.pt, events.b_cands.pt)
 
     return events
 

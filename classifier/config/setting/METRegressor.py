d82759c12d84af5fa368bfaa7275f9241f06d87a
Aniket Khanal <aniketk@falcon.phys.cmu.edu>
2026-04-01 09:34:52 -0400

    update input trees

--- classifier/config/setting/METRegressor.py
+++ classifier/config/setting/METRegressor.py
@@ -2,18 +2,17 @@ from enum import IntEnum
 from src.classifier.task import GlobalSetting
 
 class InputBranch(GlobalSetting):
-    "Name of branches in the input root file"
-    feature_bJetCand: list[str] = ["pt", "eta", "phi", "mass", "btagScore"]  
-    feature_nonbJetCand: list[str] = ["pt", "eta", "phi", "mass", "attn_score"]
+    "Name of branches in the input root file for MET regressor"
+    feature_bJetCand: list[str] = ["pt", "eta", "phi", "mass", "btagScore"]
+    feature_nonbJetCand: list[str] = ["pt", "eta", "phi", "mass"]
     feature_leadingLep: list[str] = ["pt", "eta", "phi", "mass", "isE", "isM"]
     feature_MET: list[str] = ["pt", "phi"]
     feature_ancillary: list[str] = ["HT", "njets", "nsoftjets", "year"]
     feature_genNu: list[str] = ["pt", "eta", "phi"]
     feature_genLepW: list[str] = ["onShell", "genLepWmass"]
     feature_true_nbjet_flat: list[str] = ["0", "1", "2", "3"]
-    feature_regressed_nu: list[str] = ["px", "py", "pz"]
     nbJetCand: int = 2
-    nnonbJetCand: int = 4 # nominal: 2, lowpt: 4
+    nnonbJetCand: int = 4
 
     @classmethod
     def get__feature_bJetCand(cls, var: list[str]):
@@ -47,34 +46,20 @@ class InputBranch(GlobalSetting):
     def get__feature_true_nbjet_flat(cls, var: list[str]):
         return [f"true_nbjet_flat_{f}" for f in var]
 
-    @classmethod
-    def get__feature_regressed_nu(cls, var: list[str]):
-        return [f"regressed_nu_{f}" for f in var]
-
 
 class Input(GlobalSetting):
-    "Name of the keys in the input batch."
+    "Name of the keys in the input batch for MET regressor."
     label: str = "label"
     weight: str = "weight"
     true_nbjet_flat: str = "true_nbjet_flat"
     bJetCand: str = "bJetCand"
-    nonbJetCand: str = "nonbJetCand" 
+    nonbJetCand: str = "nonbJetCand"
     leadingLep: str = "leadingLep"
     MET: str = "MET"
     ancillary: str = "ancillary"
     genNu: str = "genNu"
     genLepW: str = "genLepW"
-    regressed_nu: str = "regressed_nu"            # (n, 3): regressed neutrino px, py, pz from MET regressor
 
-class Output(GlobalSetting):
-    "Name of the keys in the output batch."
-    hh_raw: str = "hh_raw"
-    tt_raw: str = "tt_raw"
-    ww_raw: str = "ww_raw"
-    hh_prob: str = "hh_prob"
-    tt_prob: str = "tt_prob"
-    ww_prob: str = "ww_prob"
-    
 # create indeces map to different regions
 class MassRegion(IntEnum):
     ALL = 0b00 # neither signal nor CR

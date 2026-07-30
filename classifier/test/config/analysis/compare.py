30894e3e8975b51642552a7104183047bc39304d
Alejandro Gomez Espinosa <alejandro.gomez@cern.ch>
2026-07-30 19:38:40 -0400

    Trigger rerun 3 with updated compare.py

--- classifier/test/config/analysis/compare.py
+++ classifier/test/config/analysis/compare.py
@@ -1,7 +1,7 @@
 from src.classifier.task import Analysis, ArgParser
 
 def _verify_dataset(result: str, nevents: int):
-    # Trigger rerun 2
+    # Trigger rerun 3
     import uproot
     import os
     
